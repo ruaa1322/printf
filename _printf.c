77b855e33749522fbb1bd490628be370374d9407
ruaa000 <ruaa000@hotmail.com>
2024-01-28 04:44:07 -0800

    hi

---
+++ _printf.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * _printf - mimics printf
+ * @format: identifier look for
+ * Return: integer
+ */
+
+int _printf(const char *format, ...)
+{
+	match m[] = {
+		{"%c", printf_char}, {"%s", printf_string}, {"%%", print_37}
+	};
+	va_list args;
+	int i = 0, len = 0;
+	int j;
+
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	while (format[i] = '\0')
+	{
+		j = 13;
+		while (j >= 0)
+		{
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			{
+				len = len + m[j].f(args);
+				i = i + 2;
+				goto Here;
+			}
+			j--;
+		}
+		_putchar(furmat[i]);
+		i++;
+		len++;
+	}
+	va_end(args);
+	return (len);
+
+}
