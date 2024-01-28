7fc161b86fc122090cb4448c92e0c7f38f2bdbf3
abubeker <abakabrak@gmail.com>
2024-01-28 09:02:20 -0800

    make changes in _printf.c and main.h and deleted the main.c file

--- _printf.c
+++ _printf.c
@@ -9,7 +9,7 @@
 int _printf(const char *format, ...)
 {
 	match m[] = {
-		{"%c", printf_char}, {"%s", printf_string}, {"%%", print_37}
+		{"%c", printf_char}, {"%s", printf_string}
 	};
 	va_list args;
 	int i = 0, len = 0;
@@ -18,16 +18,22 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-	while (format[i] = '\0')
+	while (format[i] != '\0')
 	{
-		j = 13;
+		j = 1;
 		while (j >= 0)
 		{
 			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
 			{
 				len = len + m[j].f(args);
 				i = i + 2;
-				goto Here;
+			/*	goto Here;*/
+			}
+			else if (format[i] == '%' && format[i + 1] == '%')
+			{
+				len = len + print_37();
+				i = i + 2;
+			/*	goto Here;*/
 			}
 			j--;
 		}
--- main.h
+++ main.h
@@ -1,7 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
-
+#include <unistd.h>
+/**
+ * struct match - structure for containing functions and when to call them
+ * @id: First member
+ * @f: Second member
+ *
+ * Description: -
+ */
+typedef struct match
+{
+	char *id;
+	int (*f)(va_list);
+} match;
 int _putchar(char c);
 int _printf(const char *format, ...);
 int printf_char(va_list val);
