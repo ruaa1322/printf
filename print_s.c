35f825067019bc6f8e142b3b763e1f72fbab6e21
ruaa000 <ruaa000@hotmail.com>
2024-01-28 03:28:52 -0800

    ff

--- print_s.c
+++ print_s.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 /**
  * printf_string - print a string.
  * @val: argument
--- print_str.c
+++ print_str.c
@@ -2,26 +2,27 @@
 /**
  * _strlen - Reyurns the length of a string.
  * @str: string pointer
- * Return: 1
+ * Return: i
  */
 int _strlen(char *str)
 {
 	int i;
 
 	for (i = 0; str[i] != 0; i++)
-
-		return (i);
+		;
+	return (i);
 }
+
 /**
  *  _strlenc - strlen function but applied for constant char pointer str
  *  @str: char pointer
- *  Return: 1
+ *  Return: i
  */
 int _strlenc(const char *str)
 {
 	int i;
 
 	for (i = 0; str[i] != 0; i++)
-
-		return (i);
+		;
+	return (i);
 }
